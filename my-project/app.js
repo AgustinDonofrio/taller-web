96b4f879eda8589d72b9ec057d34b3db9c39c23b
Ignacio Ciano <nachociano2@gmail.com>
2024-07-01 19:18:42 -0300

    update vista productos de usuario y comerciante

--- my-project/app.js
+++ my-project/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var methodOverride = require('method-override');
+//var multer = require('multer');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users.routes');
