1bf7a728d09aea1c9ebce1ce343db5b0473424ee
Cecho99 <tomiceschini99@gmail.com>
2024-07-03 19:40:46 -0300

    Prototipo 1

--- my-project/models/Order.js
+++ my-project/models/Order.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const orderSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'user',
+        ref: 'User',
         required: true
     },
     commerce: {
